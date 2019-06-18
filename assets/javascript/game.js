cb36574e93a3e242b0b49f639d691992978e5745
Joshua Kelly <jdkelly2015@gmail.com>
2019-06-18 17:03:12 -0400

    i really tried my best. 6 hours a day over the weekend. i just dont understand. i cant get Your Guesses to list out. i cant get the letter change every time it matches userInput

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //Globabl variables. alphabet to generate random letters to guess. other variables to track changing values
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //tracks wins 
 var wins = 0;
 //tracks losses 
@@ -11,37 +11,48 @@ var guessesLeft = 10;
 var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log(letter)
 
+// function letter(length) {
+//     var result = '';
+//     for (var i = 0; i < length; i++) {
+//         result += alphabet[Math.floor(Math.random() * alphabet.length)];
+//     }
+//     return result;
+// }
+// console.log(letter(1));
 
 //listening for event of onkeyup
 document.onkeyup = function (event) {
 
     //put the value of pressed key inside guesses variable
-    var guesses = event.key;
+    var userInput = event.key;
 
     //if guesses specifically = letter wins increment by 
-    if (guesses === letter) {
+    if (userInput === letter) {
         wins++;
-        // change letter if correctly guessed 
-        
 
-        //otherwise guesses decrements
+        // i need a function to call on so i can replace the original letter 
+
+
     }
+    //otherwise guessesLeft decrements
     else {
         guessesLeft--;
-        
+
     }
     //2nd if statement to handle the amount of guesses you get
     if (guessesLeft === 0) {
         losses++
         guessesLeft = 10;
-        //also need to change letter at 0 guesses left  
+
     }
+
 
     // this displays to my divs. had to type string in here cuz .innerHTML rewrites entire div
     document.getElementById("letter").innerHTML = "Guess what letter I'm thinking of ";
-    document.getElementById("wins").innerHTML = "Wins: " + wins;
-    document.getElementById("losses").innerHTML = "Losses: " + losses;
-    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
-    document.getElementById("guesses").innerHTML = "Your Guesses So Far: " + guesses;
+    document.getElementById("wins").innerHTML = "Wins: " + wins; //increments per match 
+    document.getElementById("losses").innerHTML = "Losses: " + losses; // decremnts losses inside div
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft; //puts my guessesLeft to the desired div
+    document.getElementById("guesses").innerHTML = "Your Guesses So Far: " + userInput   //list all guesses here
+
 
 }
